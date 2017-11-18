de896a7d4873d47eccbd52a6c15528f4a4a149fd
Fernanda Vitorio <fersvitorio@gmail.com>
2017-11-18 16:15:19 -0200

    Titulo da pagina dinamico está funcionando

--- ClientApp/app/app.shared.module.ts
+++ ClientApp/app/app.shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, ActivatedRoute, Router } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { LoginComponent } from './components/login/login.component';
+import { Title, BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
     declarations: [
@@ -16,20 +18,26 @@ import { CounterComponent } from './components/counter/counter.component';
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        LoginComponent
     ],
     imports: [
+        BrowserModule,
         CommonModule,
         HttpModule,
         FormsModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'home', component: HomeComponent, data: { title: 'Home' } },
+            { path: 'counter', component: CounterComponent, data: { title: 'Counter' } },
+            { path: 'fetch-data', component: FetchDataComponent, data: { title: 'Fetch' } },
+            { path: 'login', component: LoginComponent, data: { title: 'Login' } },
             { path: '**', redirectTo: 'home' }
         ])
-    ]
+    ],
+    providers: [
+        Title
+          ],
 })
 export class AppModuleShared {
 }
--- ClientApp/app/components/app/app.component.ts
+++ ClientApp/app/components/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
 
 @Component({
     selector: 'app',
@@ -6,4 +11,22 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+    public constructor(private router: Router,    private activatedRoute: ActivatedRoute,        private titleService: Title) {
+
+    }
+
+    ngOnInit() {
+        this.router.events
+        .filter((event) => event instanceof NavigationEnd)
+        .map(() => this.activatedRoute)
+        .map((route) => {
+          while (route.firstChild) route = route.firstChild;
+          return route;
+        })
+        .filter((route) => route.outlet === 'primary')
+        .mergeMap((route) => route.data)
+        .subscribe((event) => this.titleService.setTitle('Frota - ' + event['title']));
+    }
+    
 }
