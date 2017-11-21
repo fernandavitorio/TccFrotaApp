41123a296644a5d06847d1620ff89afce86fcbec
Fernanda Vitorio <fersvitorio@gmail.com>
2017-11-21 21:28:15 -0200

    inicio desenvolvimento apontamentos

---
+++ ClientApp/app/home/apontamentos/apontamento.service.ts
@@ -0,0 +1,19 @@
+interface Apontamento {
+    id: number;
+    dtAtualizacao: Date;
+    aditionalInformation: number;
+    setor: string;
+    tipo: string;
+    veiculoId: number;
+    veiculoIdentificador: string;
+    veiculoPlaca: string;
+    motoristaId: number;
+    motoristaNome: string;
+    coletor1Id: number;
+    coletor1Nome: number;
+    coletor2Id: number;
+    coletor2Nome: number;
+    coletor3Id: number;
+    coletor3Nome: number;
+    apontamentoInicialId: number;
+}
